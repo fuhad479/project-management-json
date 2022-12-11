222c2bfbfb44c654e5b46048bc7ac6efc2071111
fuhad479 <fuhadhasan479@gmail.com>
2022-12-11 15:16:28 +0600

    added some basic data to server

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ server.db = router.db
 
 const rules = auth.rewriter({
     // Permission rules
-    users: 600
+    // users: 600
 })
 
 server.use(middlewares)
 server.use(rules)
 server.use(auth)
 server.use(router)
-server.listen(3000, () => {
+server.listen(5000, () => {
     console.log('JSON Server is running')
 })
